43a564d989482ab5513b3cb31624414220650fce
dmlaziuk <dm.laziuk@gmail.com>
2017-12-10 14:33:41 +0300

    production.rb doesn't work

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,3 +22,10 @@ set :sidekiq_role, :sidekiq
 set :puma_preload_app, true
 set :puma_init_active_record, true
 #set :port, '3000'
+
+# production.rb file
+set :rails_env, 'production'
+set :puma_threads, [4, 12]
+set :puma_workers, 2
+
+server '18.217.123.149', user: 'deployer', roles: %w[app web db sidekiq]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
-set :rails_env, 'production'
-set :puma_threads, [4, 12]
-set :puma_workers, 2
+#set :rails_env, 'production'
+#set :puma_threads, [4, 12]
+#set :puma_workers, 2
 
-server '18.217.123.149', user: 'deployer', roles: %w[app web db sidekiq]
+#server '18.217.123.149', user: 'deployer', roles: %w[app web db sidekiq]
