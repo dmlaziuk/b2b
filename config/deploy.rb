d0e4185b89a997324970452fbbf18b784b90ba2a
dmlaziuk <dm.laziuk@gmail.com>
2017-12-10 14:25:50 +0300

    b2b

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,15 +9,15 @@ set :deploy_to, '/var/www/b2b'
 set :ssh_options, forward_agent: true
 set :pty, false
 
-set :linked_files, %w[config/database.yml]
+set :linked_files, %w[config/database.yml config/redis.yml]
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets public/system public/assets]
 
 set :rvm_ruby_version, '2.4.1@b2b'
 
-#set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
-#set :sidekiq_processes, 2
-#set :sidekiq_log, "#{current_path}/log/sidekiq.log"
-#set :sidekiq_role, :sidekiq
+set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
+set :sidekiq_processes, 2
+set :sidekiq_log, "#{current_path}/log/sidekiq.log"
+set :sidekiq_role, :sidekiq
 
 set :puma_preload_app, true
 set :puma_init_active_record, true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,4 +2,4 @@ set :rails_env, 'production'
 set :puma_threads, [4, 12]
 set :puma_workers, 2
 
-server '18.217.123.149', user: 'deployer', roles: %w[app web db]
+server '18.217.123.149', user: 'deployer', roles: %w[app web db sidekiq]
